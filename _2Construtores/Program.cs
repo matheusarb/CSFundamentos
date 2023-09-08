1089ee0b54d649bd007eff4b63673ebc77ec6349
Matheus <mat.araujoribeiro@gmail.com>
2023-09-07 22:04:07 -0300

    construtores e this

--- _2Construtores/Program.cs
+++ _2Construtores/Program.cs
@@ -5,21 +5,24 @@
 //3.1. Caso não possua parâmetros, o C# utiliza o construtor padrão para instanciar a classe. Nenhuma classe pode ser instanciada sem o construtor
 
 
-Aluno aluno = new Aluno("Matheus Ribeiro", 31, "Masc", "t");
+Aluno aluno = new Aluno("Leuris", 31, "Masc", "t");
 
-
-Console.WriteLine(aluno.Nome == null ? "nulo" : aluno.Nome);
+Console.WriteLine(aluno.Nome ?? "null");
 Console.WriteLine(aluno.Idade == 0 ? "zero" : aluno.Idade);
 Console.WriteLine(aluno.Sexo == null ? "nulo" : aluno.Sexo);
 Console.WriteLine(aluno.Aprovado == null ? "nulo" : aluno.Aprovado);
 
+
 public class Aluno
 {
-    public Aluno(string nome) => Nome = nome;
-
-    public Aluno(string nome, int idade, string sexo, string aprovado) : this(nome)
+    public Aluno(string nome, int idade)
     {
+        Nome = nome;
         Idade = idade;
+    }
+
+    public Aluno(string nome, int idade, string sexo, string aprovado) : this(nome, idade) //a atribuição do parâmetro 'nome' nesse construtor é obtida invocando o valor passado ao outro construtor
+    {
         Sexo = sexo;
         Aprovado = aprovado;
     }
