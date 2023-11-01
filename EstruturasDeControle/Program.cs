21d80de75692372a652d0db5e2bff2f3ff017740
Matheus <mat.araujoribeiro@gmail.com>
2023-11-01 16:01:16 -0300

    [add] algorithm training

--- EstruturasDeControle/Program.cs
+++ EstruturasDeControle/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
 
 //// escrever um switch case para 3 parcelas em cima do valor de 800 reais
 
@@ -26,29 +26,63 @@
 //        break;
 //}
 
-int cargo, funcao;
-Console.WriteLine("Você é programador(1) ou gerente(2)?");
-cargo = Convert.ToInt32(Console.ReadLine());
+// DO WHILE
+//var i = 0;
+//do
+//{
+//    Console.WriteLine($"O valor de i é: {i}");
+//    i++;
+//    if(i == 5)
+//    {
+//        Console.WriteLine($"O valor de i é: {i}.O programa irá encerrar");
 
-switch (cargo)
-{
-    case 1:
-        Console.WriteLine("Você é Junior(1) ou Senior(2)?");
-        funcao = Convert.ToInt32(Console.ReadLine());
-        switch (funcao)
-        {
-            case 1:
-                Console.WriteLine("Olá, juninho!");
-                break;
-            case 2:
-                Console.WriteLine("Olá, senior");
-        break;
-        }
-        break;
-    case 2:
-        Console.WriteLine("Bem vindo gerencia");
-        break;
-    default:
-        Console.WriteLine("jogou onde?");
-        break;
-}
+//        break;
+//    }
+//} while(i < 10);
+
+//TABOADA
+//int numero, resultado;
+
+//Console.WriteLine("Digite um nº de 1 a 9");
+//numero = Convert.ToInt32(Console.ReadLine());
+//    if(!(numero > 0 && numero < 10))
+//    {
+//        Console.WriteLine("Opção inválida. O programa encerrará");
+//        Environment.Exit(0);
+//    }
+
+//for (var i = 1; i < 10; i++)
+//{
+//    resultado = numero * i;
+//    Console.WriteLine($"{numero} * {i} = {resultado}");
+//}
+
+//INSTRUCAO WHILE
+
+
+//int cargo, funcao;
+//Console.WriteLine("Você é programador(1) ou gerente(2)?");
+//cargo = Convert.ToInt32(Console.ReadLine());
+
+//switch (cargo)
+//{
+//    case 1:
+//        Console.WriteLine("Você é Junior(1) ou Senior(2)?");
+//        funcao = Convert.ToInt32(Console.ReadLine());
+//        switch (funcao)
+//        {
+//            case 1:
+//                Console.WriteLine("Olá, juninho!");
+//                break;
+//            case 2:
+//                Console.WriteLine("Olá, senior");
+//        break;
+//        }
+//        break;
+//    case 2:
+//        Console.WriteLine("Bem vindo gerencia");
+//        break;
+//    default:
+//        Console.WriteLine("jogou onde?");
+//        break;
+//}
