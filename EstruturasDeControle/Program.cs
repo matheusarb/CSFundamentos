2e5d621be1115bb8b21cf4cb95f5a81367f3772f
Matheus <mat.araujoribeiro@gmail.com>
2023-11-02 16:30:59 -0300

    [add]break, continue

--- EstruturasDeControle/Program.cs
+++ EstruturasDeControle/Program.cs
@@ -58,7 +58,11 @@
 //}
 
 //INSTRUCAO WHILE
-
+//var i = 0;
+//while(i < 6){
+//    Console.WriteLine(i);
+//    i++;
+//}
 
 //int cargo, funcao;
 //Console.WriteLine("Você é programador(1) ou gerente(2)?");
@@ -75,8 +79,8 @@
 //                Console.WriteLine("Olá, juninho!");
 //                break;
 //            case 2:
-//                Console.WriteLine("Olá, senior");
-//        break;
+//                Console.WriteLine("Olá, sênior");
+//                break;
 //        }
 //        break;
 //    case 2:
@@ -86,3 +90,25 @@
 //        Console.WriteLine("jogou onde?");
 //        break;
 //}
+
+//BREAK E CONTINUE
+//for (var i = 0; i <= 5; i++)
+//{
+//    if (i == 2)
+//    {
+//        continue;
+//    }
+//    List<int> arr = new();
+//    arr.Add(i);
+//    Console.Write($"{i} ");
+//}
+
+//EXERCICIOS
+//1.
+//int n1, n2, n3;
+//n1 = 654;
+//n2 = 646;
+//n3 = 653;
+
+//var maxNum = Math.Max(n1, (Math.Max(n2, n3)));
+//Console.WriteLine(maxNum);
