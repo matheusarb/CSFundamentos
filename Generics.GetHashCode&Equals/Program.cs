e9fd87c6b30b2736a689865f82b23abed5481b34
Matheus <mat.araujoribeiro@gmail.com>
2023-11-03 22:33:58 -0300

    [add] generics.GetHashCode() adn Equals()

---
+++ Generics.GetHashCode&Equals/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("GetHashCode e Equals\n---------\n");
+
+var pessoa1 = new Pessoa(123, "Matheus");
+var pessoa2 = new Pessoa(123, "Matheus");
+var pessoa3 = new Pessoa(234, "Luiggi");
+
+Console.WriteLine(pessoa1.Equals(pessoa2));
+Console.WriteLine(pessoa1.Equals(pessoa3));
+
+public class Pessoa
+{
+    public Pessoa(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        if(obj == null) return false;   
+        if(obj is not Pessoa) return false;
+
+        var other = (Pessoa)obj;
+
+        return Id.Equals(other.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
