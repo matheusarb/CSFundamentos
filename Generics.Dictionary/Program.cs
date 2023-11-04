76ed0393aa664819d567deb6fcd33ca8102e0224
Matheus <mat.araujoribeiro@gmail.com>
2023-11-04 10:48:40 -0300

    [add] generics dictionary

---
+++ Generics.Dictionary/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Dictionary\n----------\n");
+
+//1. cada chave em um Dictionary<TKey, TValue> deve ser exclusiva (não podem se repetir)
+//2. Uma chave NÃO PODE ser null, mas um valor pode, desde que seu tipo (TValue) seja tipo de referência (string, object...)
+
+Dictionary<string, int> dic1 = new Dictionary<string, int>();
+dic1.Add("Primeiro", 1);
+dic1.Add("Segundo", 20);
+Console.WriteLine(dic1.Values);
--- Generics.GetHashCode&Equals/Program.cs
+++ Generics.GetHashCode&Equals/Program.cs
@@ -1,36 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("GetHashCode e Equals\n---------\n");
 
-var pessoa1 = new Pessoa(123, "Matheus");
-var pessoa2 = new Pessoa(123, "Matheus");
-var pessoa3 = new Pessoa(234, "Luiggi");
+//var pessoa1 = new Pessoa(123, "Matheus");
+//var pessoa2 = new Pessoa(123, "Matheus");
+//var pessoa3 = new Pessoa(234, "Luiggi");
 
-Console.WriteLine(pessoa1.Equals(pessoa2));
-Console.WriteLine(pessoa1.Equals(pessoa3));
+//Console.WriteLine(pessoa1.Equals(pessoa2));
+//Console.WriteLine(pessoa1.Equals(pessoa3));
 
-public class Pessoa
-{
-    public Pessoa(int id, string nome)
-    {
-        Id = id;
-        Nome = nome;
-    }
 
-    public int Id { get; set; }
-    public string Nome { get; set; }
 
-    public override bool Equals(object? obj)
-    {
-        if(obj == null) return false;   
-        if(obj is not Pessoa) return false;
+//public class Pessoa
+//{
+//    public Pessoa(int id, string nome)
+//    {
+//        Id = id;
+//        Nome = nome;
+//    }
 
-        var other = (Pessoa)obj;
+//    public int Id { get; set; }
+//    public string Nome { get; set; }
 
-        return Id.Equals(other.Id);
-    }
+//    public override bool Equals(object? obj)
+//    {
+//        if (obj == null) return false;   
+//        if (obj is not Pessoa) return false;
 
-    public override int GetHashCode()
-    {
-        return Id.GetHashCode();
-    }
-}
+//        var other = (Pessoa)obj;
+
+//        return Id.Equals(other.Id);
+//    }
+
+//    public override int GetHashCode()
+//    {
+//        return Id.GetHashCode();
+//    }
+//}
